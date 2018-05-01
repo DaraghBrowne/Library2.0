9f16664c3fd42a1f08caf16be2610d506f8dfd21
eriky15 <eriky15@student.uia.no>
2018-05-01 23:49:44 +0100

    test

---
+++ Library/Controllers/About/AboutController .cs
@@ -0,0 +1,17 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+	public class AboutController : Controller
+	{
+		// GET: Info
+		public ActionResult Info() => View();
+
+
+	}
+}
